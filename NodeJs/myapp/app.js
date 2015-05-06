88013b5b8f97f8bd6e41047a19434a9bcc9874ed
zhaosir <zhaozijian@outlook.com>
2015-05-06 14:48:16 +0800

    zzj

--- NodeJs/myapp/app.js
+++ NodeJs/myapp/app.js
@@ -27,8 +27,8 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit:'1mb'}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
--- NodeJs/myapp/routes/index.js
+++ NodeJs/myapp/routes/index.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/:age', function(req, res, next) {
+  console.log("get param-------:%s,%s",req.query.q,req.param("q"));
+  console.log("url param-------:%s",req.param("age"));
   res.render('index', { title: 'Express' });
+});
+
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+
+router.post('/',multipartMiddleware,function(req,res,next){
+	console.log("post param------:%s",req.param('name'));
+	console.log("body param------:%s",req.body.name);
+	//res.render('index', { title: 'Express' });
+	res.send(req.body);
 });
 
 module.exports = router;
